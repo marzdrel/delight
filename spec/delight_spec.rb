252f4d7203f541cc6f228bb536e7d664284f45ea
Marz Drel <marzdrel@users.noreply.github.com>
2025-04-04 15:34:42 +0200

    Initial commit

---
+++ spec/delight_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Delight do
+  it "has a version number" do
+    expect(Delight::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
