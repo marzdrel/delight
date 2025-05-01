37ac140b49c8abf915db69f6261d3e763d03c099
Marz Drel <marzdrel@users.noreply.github.com>
2025-05-01 18:48:41 +0100

    Add detect_by! implementation and specs

--- lib/delight/delight_enumerator/detect_by.rb
+++ lib/delight/delight_enumerator/detect_by.rb
@@ -8,6 +8,12 @@ module Delight
           collection_matcher(element, **)
         end
       end
+
+      def detect_by!(**)
+        detect! do |element|
+          collection_matcher(element, **)
+        end
+      end
     end
   end
 end
--- spec/delight/delight_enumerator/detect_by_spec.rb
+++ spec/delight/delight_enumerator/detect_by_spec.rb
@@ -57,5 +57,57 @@ module Delight
           .to eq stub.new("John", 30)
       end
     end
+
+    describe "#detect_by!" do
+      it "selects using a method" do
+        stub = Struct.new(:name, :age)
+
+        data = [
+          stub.new("John", 30),
+          stub.new("Jane", 25),
+          stub.new("Alice", 30),
+          stub.new("Doe", 40),
+        ]
+
+        expect(data.detect_by!(age: 30))
+          .to eq stub.new("John", 30)
+      end
+
+      it "fails on invalid method name" do
+        stub = Struct.new(:name, :age)
+
+        data = [
+          stub.new("John", 30),
+          stub.new("Doe", 40),
+        ]
+
+        expect { data.detect_by!(lastname: 30) }
+          .to raise_error NoMethodError, /undefined method/
+      end
+
+      it "returns no value when no match" do
+        stub = Struct.new(:name, :age)
+
+        data = [
+          stub.new("John", 30),
+          stub.new("Doe", 40),
+        ]
+
+        expect { data.detect_by!(age: 10) }
+          .to raise_error Delight::DelightEnumerator::DetectBang::ElementNotFound
+      end
+
+      it "matches on all passed fields" do
+        stub = Struct.new(:name, :age)
+
+        data = [
+          stub.new("John", 30),
+          stub.new("Doe", 30),
+        ]
+
+        expect(data.detect_by!(age: 30, name: "John"))
+          .to eq stub.new("John", 30)
+      end
+    end
   end
 end
