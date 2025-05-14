f4b1f48ecc820a09099f220b3f2a20e0715f9dae
Marz Drel <marzdrel@users.noreply.github.com>
2025-05-14 17:26:23 +0200

    Add basic docs for SelectBy

--- lib/delight/enumerator/select_by.rb
+++ lib/delight/enumerator/select_by.rb
@@ -1,6 +1,29 @@
 module Delight
   module Enumerator
     module SelectBy
+      # Filter colleciton elements using method calls and values passed as
+      # keyword arguments.
+      #
+      # @return [Array] containingg elements that match the passed arguments.
+      #
+      # @example
+      #   ["String", 1, 3, :symbol, 'string'].select_by(class: String)
+      #     # => ["String", 'string']
+      #
+      #   Person = Struct.new(:name, :age)
+      #
+      #   people = [
+      #     Person.new("John", 30),
+      #     Person.new("Jane", 25),
+      #     Person.new("Alice", 30),
+      #   ]
+      #
+      #   people.select_by(age: 30)
+      #   # => [
+      #   #  Person.new("John", 30),
+      #   #  Person.new("Alice", 30),
+      #   # ]
+      #
       def select_by(**)
         select do |element|
           collection_matcher(element, **)
